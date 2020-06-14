39678061a1895a0b993b81410f40764cef6f4015
Steffield <steffilebby@gmail.com>
2020-06-14 17:05:58 -0400

    fade effect and type effect added

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,21 @@
 // navbar change when scrolling
+let i = 0;
+let txt = "Full Stack Web Developer";
+var speed = 50;
+
+const typeWriter = () => {
+  if (i < txt.length) {
+    document.getElementById("jobSpan").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+};
 typeWriter();
+
 window.onscroll = function () {
   myFunction();
 };
-function myFunction() {
+const myFunction = () => {
   const navbar = document.getElementById("navbar");
   if (
     document.body.scrollTop > 100 ||
@@ -13,26 +25,14 @@ function myFunction() {
   } else {
     navbar.className = navbar.className.replace(" card animate-top", "");
   }
-}
+};
 
 // toggle menu for small screens
-function toggleFunction() {
+const toggleFunction = () => {
   const x = document.getElementById("navFixed");
   if (x.className.indexOf("show") == -1) {
     x.className += " show";
   } else {
     x.className = x.className.replace(" show", "");
   }
-}
-
-const i = 0;
-const txt = "Full Stack Web Developer";
-var speed = 50;
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("jobSpan").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
+};
